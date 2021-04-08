9b697dfdebb2d0cf5d979f86fa1a0e810a814569
Olena Rachek <nenka@i.ua>
2021-04-08 14:45:07 +0300

    exercise2a

---
+++ src/main/java/com/company/exercise2a/Test.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 0;
+        n = n * n;
+        String a = Integer.toString(n);
+
+        if (a.charAt(i) == '3') ;
+        {
+            System.out.println("Цифра 3 входит в запись");
+        }
+    }
+}
