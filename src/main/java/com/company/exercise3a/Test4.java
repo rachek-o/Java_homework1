70c265b4f8fb2d2a46f826c0d21f25ca9e5d06de
Olena Rachek <nenka@i.ua>
2021-04-08 14:56:57 +0300

    exercise3a

---
+++ src/main/java/com/company/exercise3a/Test4.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+
+public class Test4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the radius of the circle: ");
+        double radius = sc.nextDouble();
+        System.out.println("Perimeter is = " + (2 * radius * Math.PI));
+        System.out.println("Area is = " + (Math.PI * radius * radius));
+    }
+}
