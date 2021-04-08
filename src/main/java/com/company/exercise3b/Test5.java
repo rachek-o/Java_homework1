53a952553670970a9dbc2ea7dbc2313b28b610e8
Olena Rachek <nenka@i.ua>
2021-04-08 15:00:03 +0300

    exercise3b

---
+++ src/main/java/com/company/exercise3b/Test5.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test5 {
+    public static void main(String[] args) throws IOException {
+        String name;
+        String address;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("What is your name?");
+        name = br .readLine();
+        System.out.println("Where are you live," + name + "?");
+        address = br .readLine();
+        System.out.println(name + ", " + address);
+    }
+}
